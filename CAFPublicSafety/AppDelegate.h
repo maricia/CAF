ece32f8446c9d98cf74073d153fa9f6ca8491cb8
Maricia Alleman <maricia@Maricias-MacBook-Pro.local>
2013-10-07 19:16:17 -0500

    Initial Commit

---
+++ CAFPublicSafety/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CAFPublicSafety
+//
+//  Created by Maricia Alleman on 10/7/13.
+//  Copyright (c) 2013 maricia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CAFPublicSafety/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CAFPublicSafety
+//
+//  Created by Maricia Alleman on 10/7/13.
+//  Copyright (c) 2013 maricia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
